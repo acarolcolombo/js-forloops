215fc17c4c80b34eaf7bdaf2c860cefa48902021
unknown <acarolcolombo@gmail.com>
2019-04-13 16:09:17 -1000

    exerc 5 and 6

--- exercises.js
+++ exercises.js
@@ -87,6 +87,15 @@ Write a for-loop that will push even numbers to the `evenNumArr` array. We want 
 Console.log your results.
 */
 
+var evenNumArr = [];
+
+for (var i = 0; i<=100; i++){
+   if (i%2 === 0){
+      evenNumArr.push(i);
+   }
+ }
+
+ console.log(evenNumArr);
 
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
@@ -94,7 +103,15 @@ Declare a variable named `oddSum` and assign it to the Number value 0.
 Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
 Console.log your results.
 */
-  
+
+var oddSum = 0
+
+for (var i=1; i<=100; i++){
+   if (i%2 === 1){
+      oddSum += i;
+   }
+}
+console.log(oddSum);
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
